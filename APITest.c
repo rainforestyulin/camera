452da10c0b8e052e887d2e4a2bd39113e3929ab1
rainforestyulin <1727987696@qq.com>
2023-03-03 16:03:37 +0800

    添加了新测试

--- APITest.c
+++ APITest.c
@@ -1,10 +1,16 @@
 #include "DHCameraControl.h"
+#include "CameraParam.h"
+
 #include <stdio.h>
+#include <stdlib.h>
+
 void GX_CC_InitLib();
 const char* GX_CC_GetSDKVersion();
 int main()
 {
+    GX_CC_DEVICE_INFO_LIST deviceList;
     GX_CC_InitLib();
+    GX_CC_EnumDevices(&deviceList);
     printf(GX_CC_GetSDKVersion());
     return 0;
 }
